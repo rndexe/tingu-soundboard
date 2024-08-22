c1e95d424ce74dc3abd21c1ac4199e3301613799
rndexe <himanshu.ernde@gmail.com>
2024-08-22 20:25:19 +0530

    apple splash screen

--- pwa-assets.config.js
+++ pwa-assets.config.js
@@ -1,12 +1,43 @@
-import {
-    defineConfig,
-    minimal2023Preset as preset,
-} from '@vite-pwa/assets-generator/config'
+import { defineConfig, combinePresetAndAppleSplashScreens, minimal2023Preset } from "@vite-pwa/assets-generator/config";
 
 export default defineConfig({
     headLinkOptions: {
-        preset: '2023',
+        preset: "2023",
     },
-    preset,
-    images: ['public/tingu.png'],
-})
+    preset: combinePresetAndAppleSplashScreens(
+        minimal2023Preset,
+        {
+            padding: 0.3,
+            resizeOptions: { background: "#f97316", fit: "contain" },
+            // by default, dark splash screens are exluded
+            // darkResizeOptions: { background: 'black' },
+            linkMediaOptions: {
+                // will log the links you need to add to your html pages
+                log: true,
+                // add screen to media attribute link?
+                // by default:
+                // <link rel="apple-touch-startup-image" href="..." media="screen and ...">
+                addMediaScreen: true,
+                basePath: "/",
+                // add closing link tag?
+                // by default:
+                // <link rel="apple-touch-startup-image" href="..." media="...">
+                // with xhtml enabled:
+                // <link rel="apple-touch-startup-image" href="..." media="..." />
+                xhtml: false,
+            },
+            png: {
+                compressionLevel: 9,
+                quality: 60,
+            },
+            name: (landscape, size, dark) => {
+                return `apple-splash-${landscape ? "landscape" : "portrait"}-${
+                    typeof dark === "boolean" ? (dark ? "dark-" : "light-") : ""
+                }${size.width}x${size.height}.png`;
+            },
+        },
+        ["iPhone 13"]
+    ),
+
+    images: ["public/tingu-orng.png"],
+});
--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,42 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  return {
-  plugins: [VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+    return {
+        plugins: [
+            VitePWA({
+                registerType: "autoUpdate",
+                injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+                pwaAssets: {
+                    disabled: false,
+                    config: true,
+                },
 
-    manifest: {
-      name: 'tingu soundboard',
-      short_name: 'tingu',
-      description: 'tingu soundboard',
-      theme_color: '#f97316',
-    },
+                manifest: {
+                    name: "tingu soundboard",
+                    short_name: "tingu",
+                    description: "tingu soundboard",
+                    theme_color: "#f97316",
+                },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico,m4a}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+                workbox: {
+                    globPatterns: ["**/*"],
+                    cleanupOutdatedCaches: true,
+                    clientsClaim: true,
+                },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-  base: mode === "development" ? "/" : "/tingu-soundboard/",
-  }
-})
+                includeAssets: ["**/*"],
+
+                devOptions: {
+                    enabled: false,
+                    navigateFallback: "index.html",
+                    suppressWarnings: true,
+                    type: "module",
+                },
+            }),
+        ],
+        base: mode === "development" ? "/" : "/tingu-soundboard/",
+    };
+});
